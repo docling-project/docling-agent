4096f6a5f0bb3c697f03a704350e639ef4696d5d
Peter Staar <taa@zurich.ibm.com>
2025-11-12 15:44:31 +0100

    reformatted the code
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- tests/test_agents_instantiation.py
+++ tests/test_agents_instantiation.py
@@ -2,8 +2,9 @@ import pytest
 
 
 def test_instantiate_docling_writing_agent():
-    from docling_agent.agent.writer import DoclingWritingAgent
     from mellea.backends import model_ids
+
+    from docling_agent.agent.writer import DoclingWritingAgent
 
     try:
         _ = DoclingWritingAgent(model_id=model_ids.OPENAI_GPT_OSS_20B, tools=[])
@@ -12,8 +13,9 @@ def test_instantiate_docling_writing_agent():
 
 
 def test_instantiate_docling_editing_agent():
-    from docling_agent.agent.editor import DoclingEditingAgent
     from mellea.backends import model_ids
+
+    from docling_agent.agent.editor import DoclingEditingAgent
 
     try:
         _ = DoclingEditingAgent(model_id=model_ids.OPENAI_GPT_OSS_20B, tools=[])
@@ -22,11 +24,11 @@ def test_instantiate_docling_editing_agent():
 
 
 def test_instantiate_docling_extracting_agent():
-    from docling_agent.agent.extraction import DoclingExtractingAgent
     from mellea.backends import model_ids
+
+    from docling_agent.agent.extraction import DoclingExtractingAgent
 
     try:
         _ = DoclingExtractingAgent(model_id=model_ids.OPENAI_GPT_OSS_20B, tools=[])
     except Exception as e:
         pytest.fail(f"DoclingExtractingAgent instantiation raised: {e}")
-
