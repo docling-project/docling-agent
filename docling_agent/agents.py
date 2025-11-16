eac4121ca2ffec4d9132059e3789a7832f4e16ff
Peter Staar <taa@zurich.ibm.com>
2025-11-16 06:39:04 +0100

    Added the enricher
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- docling_agent/agents.py
+++ docling_agent/agents.py
@@ -1,12 +1,14 @@
 # Public re-exports for convenience imports in examples
 from docling_agent.agent.editor import DoclingEditingAgent
 from docling_agent.agent.extractor import DoclingExtractingAgent
+from docling_agent.agent.enricher import DoclingEnrichingAgent
 from docling_agent.agent.writer import DoclingWritingAgent
 from docling_agent.logging import logger
 
 __all__ = [
     "DoclingEditingAgent",
     "DoclingExtractingAgent",
+    "DoclingEnrichingAgent",
     "DoclingWritingAgent",
     "logger",
 ]
